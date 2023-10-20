25b47b37dc3d6f4ef2db6775a226b6fae4d94099
owais17 <57414701+owais17@users.noreply.github.com>
2023-10-20 22:11:08 +0500

    Create chats_page.dart

---
+++ chats_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chats'),
+      ),
+      body: Center(
+        child: Text('Chats Page Content'),
+      ),
+    );
+  }
+}
