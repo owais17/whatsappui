427c212b5dfd90e6bccce8ea1aa9a6df63568631
owais17 <57414701+owais17@users.noreply.github.com>
2023-10-20 22:09:43 +0500

    Create call_page.dart

---
+++ import 'package:flutter/call_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CallsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calls'),
+      ),
+      body: Center(
+        child: Text('Calls Page Content'),
+      ),
+    );
+  }
+}
