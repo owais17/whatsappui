4774ecdc2421ce85784fa9a1c38822d442ac5b16
owais17 <57414701+owais17@users.noreply.github.com>
2023-10-20 22:11:35 +0500

    Create status_page.dart

---
+++ status_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatusPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Status'),
+      ),
+      body: Center(
+        child: Text('Status Page Content'),
+      ),
+    );
+  }
+}
